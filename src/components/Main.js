29ec414a8c5a3c50084eed9cb924f0eb60746ae2
FanGuo <fguo10@hawk.iit.edu>
2022-05-17 13:33:42 -0500

    [add] home page about main(article part finished)

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,9 +31,77 @@ const Main = (props) => {
                         <span>Write article</span>
                     </button>
                 </div>
-
-
             </ShareBox>
+
+
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src='/images/user.svg' alt='' />
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src='/images/ellipses.svg' alt='' />
+                        </button>
+                    </SharedActor>
+
+                    <Description>
+                        Description
+                    </Description>
+                    <SharedImage>
+                        <a>
+                            <img src='/images/shared-image.jpg' alt='' />
+                        </a>
+                    </SharedImage>
+
+                    <SocialCount>
+                        <li>
+                            <button>
+                                <img src="/images/linkedin-like-icon.svg" alt="" />
+                                <img
+                                    src="/images/linkedin-love-icon.svg"
+                                    alt=""
+                                />
+                                <img
+                                    src="/images/linkedin-clap-icon.svg"
+                                    alt=""
+                                />
+                                <span>143</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>34 comments</a>
+                        </li>
+                    </SocialCount>
+
+                    <SocialActions>
+                        <button>
+                            <img src="/images/like-icon.svg" alt="" />
+                            <span>Like</span>
+                        </button>
+
+                        <button>
+                            <img src="/images/comment-icon.svg" alt="" />
+                            <span>Comment</span>
+                        </button>
+
+                        <button>
+                            <img src="/images/share-icon.svg" alt="" />
+                            <span>Share</span>
+                        </button>
+
+                        <button>
+                            <img src="/images/send-icon.svg" alt="" />
+                            <span>Send</span>
+                        </button>
+                    </SocialActions>
+                </Article>
+            </div>
 
         </Container>
     )
@@ -52,7 +120,7 @@ const CommonCard = styled.div`
     border-radius: 5px;
     position: relative;
     border: none;
-    box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 1px rgb(0 0 0 / 20%);
+    box-shadow: 0 0 0 1px rgb(0 0 0 / 10%), 0 0 0 1px rgb(0 0 0 / 15%);
 `;
 
 
@@ -77,6 +145,7 @@ const ShareBox = styled(CommonCard)`
             display: flex;
             align-items: center;
             font-weight: 600;
+            padding-left: 15px;
         }
         &:first-child {
             display: flex;
@@ -120,5 +189,158 @@ const ShareBox = styled(CommonCard)`
     }
 `;
 
+const Article = styled(CommonCard)`
+    padding: 0;
+    margin: 0 0 8px;
+    overflow: visible;
+`;
+
+
+const SharedActor = styled.div`
+    padding-right: 40px;
+    flex-wrap: nowrap;
+    padding: 12px 16px 0;
+    margin-bottom: 8px;
+    align-items: center;
+    display: flex;
+
+    a {
+        margin-right: 12px;
+        flex-grow: 1;
+        overflow: hidden;
+        display: flex;
+        text-decoration: none;
+
+        img {
+            width: 48px;
+            height: 48px;
+        }
+
+        & > div {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            margin-left: 8px;
+            overflow: hidden;
+
+            span {
+                text-align: left;
+
+                &:first-child {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: rgba(0, 0, 0, 1);
+                }
+
+                &:nth-child(n+1) {
+                    font-size: 12px;
+                    color: rgba(0, 0, 0, 0.6);
+                }
+
+            }
+        }
+    }
+
+button{
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+}
+
+`;
+
+const Description = styled.div`
+    padding: 0 16px;
+    overflow: hidden;
+    color: rgba(0, 0, 0, 0.9);
+    font-size: 14px;
+    text-align: left;
+
+`;
+
+
+
+const SharedImage = styled.div`
+    margin-top: 8px;
+    width: 100%;
+    display: block;
+    position: relative;
+    background-color: #f9fafb;
+
+    img {
+        /* 缩放替换后的内容以保持其纵横比 */
+        object-fit: contain;
+        width: 100%;
+        height: 100%;
+
+
+    }
+`;
+
+
+const SocialCount = styled.ul`
+    line-height: 1.3;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1px solid #e9e5df;
+    list-style: none;
+
+    button {
+        align-items: center;
+        background-color: transparent;
+        border: none;
+    }
+
+    li {
+        margin-right: 5px;
+        font-size: 13px;
+        button {
+            display: flex;
+        }
+        img {
+            width: 20px;
+        }
+    }
+`;
+
+
+const SocialActions = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin: 0;
+    min-height: 40px;
+    padding: 4px 70px 8px 9px;
+
+    button {
+        display: inline-flex;
+        align-items: center;
+        /* color: #0a66c2; */
+        color: 	＃D3D3D3;
+        width: 50px;
+        height: 50px;
+        background-color: transparent;
+        border: none;
+        img {
+            width:  25px;
+            height: 25px;
+        }
+        @media (min-width: 768px) {
+            span {
+                margin-left: 8px;
+            }
+            a {
+                margin-left: 8px;
+            }
+        }
+    }
+`;
 
 export default Main;
